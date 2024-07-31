16d995b6802fef790606bf9aec4f1ca16c9b3ae1
Andres Leones Palacio <afleones@icloud.com>
2024-07-31 13:11:46 -0500

    cambiando tipo de dato numerico a phone en customers

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -32,7 +32,7 @@ class UsersController extends Controller
             'email' => 'required|email|unique:users,email',
             'password' => 'required|string|min:8',
             'address' => 'required|string|max:191',
-            'phone' => 'nullable|numeric',
+            'phone' => 'required|numeric',
         ]);
 
         if ($validator->fails()) {
